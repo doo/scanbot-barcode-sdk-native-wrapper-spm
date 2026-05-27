f8eaf3c7197956050360ad3a5ffbf877d9c53b38
StefanKScanbot <stefankrzhovski@scanbot.io>
2026-05-27 13:10:57 +0200

    Release 9.0.3

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotBarcodeSDKWrapper",
-            url: "https://download.scanbot.io/sdk/barcode-sdk-wrapper/ios/cocoapods/scanbot-barcode-sdk-wrapper-pods-9.0.2.zip",
-            checksum: "26013befbc0884ade0bff45c45d760b756171b89d4ac92c8dcb9c70a27ce9494"
+            url: "https://download.scanbot.io/sdk/barcode-sdk-wrapper/ios/cocoapods/scanbot-barcode-sdk-wrapper-pods-9.0.3.zip",
+            checksum: "4b44f94a036995ed7443556fc523748291130d8c98ca9e749df45ca6eca641e3"
         ),
         .binaryTarget(
             name: "ScanbotBarcodeScannerSDK",
-            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/pre/xcframeworks/RC9/scanbot-ios-barcode-scanner-sdk-xcframework-9.0.0.zip",
-            checksum: "35e97e3bc93576ba18171fe06768b27464686d7aed177c050f22cc321de2ccdd"
-        ),
+            url: "https://download.scanbot.io/barcode-scanner-sdk/ios/pre/xcframeworks/RC10/scanbot-ios-barcode-scanner-sdk-xcframework-9.0.0.zip",
+            checksum: "719acf6419043b80f5c109a4440567fffbd07a3351b81ba340f038c8690981fd"
+        )
     ]
 )
